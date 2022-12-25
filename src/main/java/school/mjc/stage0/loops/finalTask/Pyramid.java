bbe8eb10e68a033627ff3faf9eab44799a47c4bf
Dmitry Golub <dmgolub@mail.ru>
2022-12-25 21:25:59 +0400

    Task completed.

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int rowNumber = 0; rowNumber < cathetusLength; rowNumber++) {
+            for (int j = 0; j < cathetusLength + rowNumber; j++) {
+                if (j >= cathetusLength - rowNumber - 1 && j < cathetusLength) {
+                    System.out.print(cathetusLength - j);
+                } else if ( j >= cathetusLength) {
+                    System.out.print(j - cathetusLength + 2);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
